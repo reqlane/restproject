b9596326d70fbe822f7da5675c7d717bafe8b0b3
reqlane <107763485+reqlane@users.noreply.github.com>
2026-03-14 17:47:31 +0000

    Basic routing crud http methods

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -10,6 +10,46 @@ func main() {
 
 	port := 3000
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// fmt.Fprintf(w, "Hello Root Route")
+		w.Write([]byte("Hello Root Route"))
+		fmt.Println("Hello Root Route")
+	})
+
+	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method)
+		switch r.Method {
+		case http.MethodGet:
+			w.Write([]byte("Hello GET Method on Teachers Route"))
+			fmt.Println("Hello GET Method on Teachers Route")
+		case http.MethodPost:
+			w.Write([]byte("Hello POST Method on Teachers Route"))
+			fmt.Println("Hello POST Method on Teachers Route")
+		case http.MethodPut:
+			w.Write([]byte("Hello PUT Method on Teachers Route"))
+			fmt.Println("Hello PUT Method on Teachers Route")
+		case http.MethodPatch:
+			w.Write([]byte("Hello PATCH Method on Teachers Route"))
+			fmt.Println("Hello PATCH Method on Teachers Route")
+		case http.MethodDelete:
+			w.Write([]byte("Hello DELETE Method on Teachers Route"))
+			fmt.Println("Hello DELETE Method on Teachers Route")
+		}
+
+		w.Write([]byte("Hello Teachers Route"))
+		fmt.Println("Hello Teachers Route")
+	})
+
+	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Students Route"))
+		fmt.Println("Hello Students Route")
+	})
+
+	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Execs Route"))
+		fmt.Println("Hello Execs Route")
+	})
+
 	fmt.Println("Server is running on port:", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
